255cd4284ff3651e198db74e74aab5e3e08a7870
Lisnyk-M <swapbuffers@gmail.com>
2020-07-25 11:21:46 +0300

    changed markup

--- src/js/templating.js
+++ src/js/templating.js
@@ -5,24 +5,18 @@ const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
 };
-
-const markup = itemsTemplate(recepies);
 const refs = {
     galleryRef: document.querySelector('.js-menu'),
     switchTheme: document.querySelector('.js-switch-input')
 }
 
+const markup = itemsTemplate(recepies);
 refs.galleryRef.insertAdjacentHTML('beforeend', markup);
-
-
 
 const themeChange = (addTheme, subTheme) => {
     document.body.classList.add(addTheme);
     document.body.classList.remove(subTheme);
 }
-
-
-
 
 // console.log('refs.switchTheme: ', refs.switchTheme.checked);
 refs.switchTheme.addEventListener('change', () => {
